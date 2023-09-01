f63e51f46ffa2a071caaeb4b86c1c2a05ec8b250
maxim1955 <nikitin.maxim.99@gmail.com>
2023-09-01 16:30:25 +0300

    first commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <NuxtPage/>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style>
+.container {
+  margin: 60px auto 0;
+  padding: 1rem;
+  max-width: 900px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrap">
+    <h3 class="title">Modal window</h3>
+    <modal>
+      <p class="modal-body">
+        body
+      </p>
+      <p class="modal-footer">footer</p>
+    </modal>
+
+  </div>
+</template>
+<script setup>
+
+
+</script>
+<style scoped>
+.wrap {
+  margin: 0 auto;
+  max-width: 1440px;
+  border: 1px solid black;
+}
+
+.title {
+  text-align: center;
+  font-size: 30px;
+
+}
+</style>
+
